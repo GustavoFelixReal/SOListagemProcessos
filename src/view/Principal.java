8a0043309f40f7ef3854d0dda206345c0c645374
Gustavo Felix <gustavo.felixreal@gmail.com>
2021-08-22 17:13:29 -0300

    Tarefa em teste

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.KillController;
 
 public class Principal {
@@ -8,7 +10,38 @@ public class Principal {
 		
 		KillController kill = new KillController();
 		
-		kill.listProcess();
+		
+		
+		int option = 0;
+		
+		do {
+			try {
+				option = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção: \n 1 - Consultar Processos \n 2 - Matar processo por PID \n 3 - Matar processo por nome \n 0 - Sair"));
+			} catch (Exception error) {
+				JOptionPane.showMessageDialog(null, "Obrigado pela preferência");
+				break;
+			}
+			
+			switch (option) {
+				case 1:
+					kill.listProcess();
+					break;
+				case 2:
+					String pid = JOptionPane.showInputDialog(null, "Insira o PID");
+					kill.killProcessByPID(pid);
+					break;
+				case 3:
+					String name = JOptionPane.showInputDialog(null, "Insira o nome do processo");
+					kill.killProcessByName(name);
+					break;
+				case 0: 
+					JOptionPane.showMessageDialog(null, "Obrigado pela preferência");
+					break;
+				default:
+					JOptionPane.showMessageDialog(null, "Opção inválida");
+			} 
+			
+		} while (option != 0);
 
 	}
 
